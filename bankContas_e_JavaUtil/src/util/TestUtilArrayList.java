581eea12c3593adee9f5793edbf2fc6fc74d0b8f
PedroAraujooj <pedro.a.vieira21@gmail.com>
2023-02-09 23:40:59 -0300

    excluindo meta

--- bankContas_e_JavaUtil/src/util/TestUtilArrayList.java
+++ bankContas_e_JavaUtil/src/util/TestUtilArrayList.java
@@ -32,7 +32,7 @@ public class TestUtilArrayList {
 		ContaCorrente cp1 = new ContaCorrente(22,22,22);
 		
 		boolean bool = list.contains(cp1);
-		System.out.println(bool); // reescrevi o metodo equalss
+		System.out.println(bool); // reescrevi o metodo equals na class conta
 
 	}
 
