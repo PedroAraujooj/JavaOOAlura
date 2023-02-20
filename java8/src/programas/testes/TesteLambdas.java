8e3cd3c276f52866aa3bcd64c74e281ebccd1472
PedroAraujooj <pedro.a.vieira21@gmail.com>
2023-02-20 18:16:21 -0300

    java8 testes

---
+++ java8/src/programas/testes/TesteLambdas.java
@@ -0,0 +1,29 @@
+package programas.testes;
+
+import static java.util.Comparator.*;
+import java.util.ArrayList;
+
+import java.util.List;
+
+
+
+public class TesteLambdas {
+	public static void main(String[] args) {
+
+		List<String> palavras = new ArrayList<>();
+		palavras.add("alura online");
+		palavras.add("casa do código");
+		palavras.add("caelum");
+
+		//palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		//palavras.sort(Comparator.comparingInt(s -> s.length()));
+		palavras.sort(comparing(String::length));
+
+
+		
+		//palavras.forEach(s -> System.out.println(s));
+		palavras.forEach(System.out::println);
+		
+	}
+
+}
