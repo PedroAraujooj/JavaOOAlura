6e542d07e45bcbb315c5a7d74ed130cad177ef11
PedroAraujooj <pedro.a.vieira21@gmail.com>
2023-02-21 22:39:39 -0300

    trabalhando com OPtionals e Collectors

--- java8/src/programas/testes/TesteStream.java
+++ java8/src/programas/testes/TesteStream.java
@@ -1,6 +1,10 @@
 package programas.testes;
 
 import java.util.ArrayList;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TesteStream {
 	public static void main(String[] args) {
@@ -15,6 +19,16 @@ public class TesteStream {
 	    //cursos.sort(Comparator.comparing(Curso::getAlunos));
 
 	    System.out.println(cursos.stream().filter(c -> c.getAlunos() > 50).mapToInt(c -> c.getAlunos()).sum());
+	    cursos.stream().filter(c -> c.getAlunos() > 50).collect(Collectors.toList());
+	    System.out.println(cursos.stream().filter(c -> c.getAlunos() > 50).mapToInt(c -> c.getAlunos()).average());
+	    cursos.stream().filter(c -> c.getAlunos() > 50).collect(Collectors.toMap(c -> c.getNome(), c-> c.getAlunos()));
+	    
+	   Optional<Curso> findAny = cursos.stream().filter(c -> c.getAlunos() > 50).findAny();
+	   System.out.println(findAny.get().getAlunos());
+
+	   
+	   
+	    //System.out.println(findFirst);
 
 	    //cursos.forEach(c -> System.out.println(c.getAlunos()));
 	
